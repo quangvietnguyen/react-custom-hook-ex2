e69f0937294ca60c18dada89a6cfa5212e583849
nguye374 <nguye374@sheridancollege.ca>
2021-09-23 01:43:36 -0400

    Sep 23

--- src/App.js
+++ src/App.js
@@ -1,51 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import Section from './components/UI/Section';
+import useHttp from './hooks/use-http';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
-
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-45535-default-rtdb.firebaseio.com/tasks.json'
-      );
-
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
-  };
+  
+  const {isLoading, error, sendRequest: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = ((tasksObj) => {
+      const loadedTasks = [];
+  
+        for (const taskKey in tasksObj) {
+          loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+        }
+  
+        setTasks(loadedTasks);
+    });
+    fetchTasks({url: 'https://react-http-45535-default-rtdb.firebaseio.com/tasks.json'},transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
   };
 
+  const deleteHandler = async () => {
+    const response = await fetch(
+      'https://react-http-45535-default-rtdb.firebaseio.com/tasks.json',{
+        method : 'DELETE'
+      }
+    );
+  }
+
   return (
     <React.Fragment>
       <NewTask onAddTask={taskAddHandler} />
+      <Section>
+        <button onClick={deleteHandler}>Delete Database</button>
+      </Section>
       <Tasks
         items={tasks}
         loading={isLoading}
